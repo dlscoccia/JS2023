3334fe98e74a6ab9c423db985f99f10190092d66
Daniel Lorenzo Scoccia <66275993+dlscoccia@users.noreply.github.com>
2023-12-14 12:28:47 -0400

    Update

--- express-mongo/src/config/config.ts
+++ express-mongo/src/config/config.ts
@@ -2,9 +2,9 @@ export default {
     jwtSecret: process.env.JWT_SECRET || 'fonziSecret',
     URI:
         process.env.MONGODB_URI ||
-        'mongodb+srv://root:root@cluster0.9htba.mongodb.net/ecoquartz',
+        '',
     USER: process.env.MONGODB_USER,
     PASSWORD: process.env.MONGODB_PASSWORD,
     CLOUDINARY_URL:
-        'cloudinary://478691736723186:8JXV9QyN4JXtEHteDtTPtQX_Uys@dlscoccia',
+        '',
 };
