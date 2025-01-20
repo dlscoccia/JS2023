3c995d6084373f39cb6f1073a36b51b251adc950
dlscoccia <dlscoccia@gmail.com>
2025-01-20 14:12:10 -0400

    Added shopping cart with cookies management

--- admin-todos-nextjs15/src/app/dashboard/layout.tsx
+++ admin-todos-nextjs15/src/app/dashboard/layout.tsx
@@ -1,14 +1,16 @@
-import { Sidebar, TopMenu } from '@/components'
-import { PropsWithChildren } from 'react'
+import { Sidebar, TopMenu } from '@/components';
+import { PropsWithChildren } from 'react';
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
-  return (
-    <>
-      <Sidebar />
-      <div className='ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] min-h-screen'>
-        <TopMenu />
-        <div className='px-6 pt-6'>{children}</div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Sidebar />
+            <div className='ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] min-h-screen'>
+                <TopMenu />
+                <div className='px-6 pt-6 bg-white p-2 pb-5 m-2 rounded'>
+                    {children}
+                </div>
+            </div>
+        </>
+    );
 }
--- admin-todos-nextjs15/src/routes/routes.tsx
+++ admin-todos-nextjs15/src/routes/routes.tsx
@@ -1,19 +1,35 @@
-import { IoCalendar, IoCheckboxOutline, IoListOutline } from 'react-icons/io5'
+import {
+    IoCalendar,
+    IoCartOutline,
+    IoCheckboxOutline,
+    IoCode,
+    IoListOutline,
+} from 'react-icons/io5';
 
 export const menuItems = [
-  {
-    title: 'Dashboard',
-    icon: <IoCalendar size={30} />,
-    href: '/dashboard',
-  },
-  {
-    title: 'REST Todos',
-    icon: <IoCheckboxOutline size={30} />,
-    href: '/dashboard/rest-todos',
-  },
-  {
-    title: 'Server Actions',
-    icon: <IoListOutline size={30} />,
-    href: '/dashboard/server-todos',
-  },
-]
+    {
+        title: 'Dashboard',
+        icon: <IoCalendar size={30} />,
+        href: '/dashboard',
+    },
+    {
+        title: 'REST Todos',
+        icon: <IoCheckboxOutline size={30} />,
+        href: '/dashboard/rest-todos',
+    },
+    {
+        title: 'Server Actions',
+        icon: <IoListOutline size={30} />,
+        href: '/dashboard/server-todos',
+    },
+    {
+        title: 'Cookies',
+        icon: <IoCode size={30} />,
+        href: '/dashboard/cookies',
+    },
+    {
+        title: 'Products',
+        icon: <IoCartOutline size={30} />,
+        href: '/dashboard/products',
+    },
+];
